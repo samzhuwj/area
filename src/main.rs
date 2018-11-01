e7c5e75310b7bc4796612f715dadf5cbc9ff28d9
samzhuwj <samzhuwj@gmail.com>
2018-11-01 11:42:34 +0800

    add the annotation to derive the Debug trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -6,5 +7,5 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("rect1 is {}", rect1);
+    println!("rect1 is {:?}", rect1);
 }
